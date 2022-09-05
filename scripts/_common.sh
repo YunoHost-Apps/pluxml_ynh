a3245846e8d96210975c85bc6348ffc632984d8f
yalh76 <yalh@yahoo.com>
2022-09-05 20:06:52 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 php_dependencies="php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-xml"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="deb1 deb2 $php_dependencies"
+pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
